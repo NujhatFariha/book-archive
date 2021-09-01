b9280997c64a0903d1488130f6cfe568762fcdb9
nujhatfariha049 <nujhatfarihaa@gmail.com>
2021-09-01 23:40:46 +0600

    search field created

--- js/app.js
+++ js/app.js
@@ -1,1 +1,17 @@
-console.log("hello world");
+const searchBook = () => {
+    const searchField = document.getElementById('search-field');
+    const searchInput = searchField.value;
+    // clear all the data
+    searchField.value = ' ';
+    if (searchInput == '') {
+        return "search field cannot be empty";
+    }
+    else{
+        const url = `http://openlibrary.org/search.json?q=${searchInput}`;
+        fetch (url)
+        .then(res => res.json())
+        .then(data => console.log(data))
+        .catch(error => displayError(error));
+    }
+}
+
