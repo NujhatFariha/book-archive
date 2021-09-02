23985a9e350a0e01c0f03493a1126de78deccd9c
nujhatfariha049 <nujhatfarihaa@gmail.com>
2021-09-02 23:11:51 +0600

    displayed data

--- js/app.js
+++ js/app.js
@@ -1,45 +1,55 @@
-const totalSearch = document.getElementById('total-search-result')
+document.getElementById('spinner').style.display = "none";
+
 const searchBook = () => {
     const searchField = document.getElementById('search-field');
     const searchText = searchField.value;
-    totalSearch.innerText = searchText;
-
-    // clear all the data
-    searchField.value = ' ';
-    // if (searchText === '') {
-    //     return "search field cannot be empty";
-    // }
-    // else{
     const url = `https://openlibrary.org/search.json?q=${searchText}`;
+
+    document.getElementById('spinner').style.display = "block";
     fetch(url)
         .then(res => res.json())
-        .then(data => displaySearchResult(data.docs));
+        .then(data => {
+            //Search Result 
+            const searchResult = document.getElementById('search-result');
+            document.getElementById('spinner').style.display = "none";
+            //clear search result
+            searchResult.textContent = '';
+
+            const searchResultResult = document.createElement('div');
+            searchResultResult.classList.add('text-center');
+            
+            if (data.numFound === 0) {
+                searchResultResult.innerHTML = `
+                    <h2 class="text-danger">Sorry! Please search with a proper book name</h2>
+                `;
+            }
+            else {
+                searchResultResult.innerHTML = `
+                            <h2> Result-Found : <span style="color: rgba(22, 9, 160, 0.623)">${data.numFound}</span></h2>
+                `;
+            }
+            searchResult.appendChild(searchResultResult);
+            //clear book details...
+            searchResult.textContent = '';
+
+            //append all books
+            (data.docs).forEach((book) => {
+                const div = document.createElement('div');
+                div.classList.add('col');
+                div.innerHTML = `
+                    <div class=" card card-body text-start">
+                      <img src="https://covers.openlibrary.org/b/id/${book.cover_i}-M.jpg" class="img-fluid rounded" style="height: 250px;" alt="...">
+                          <p class="fw-bold fs-6 mt-2">Book Name : <span class="fw-normal fs-5">${book.title}<span><p>
+                          <p><span class="fw-bold fs-6">Author Name : <span class="fw-normal fs-6"> ${book.author_name[0]}</p>
+                          <p><span class="fw-bold fs-6">Publisher : <span class="fw-normal fs-6"> ${book.publisher[0]}</p>
+                          <p><span class="fw-bold fs-6">First Published Year : <span class="fw-normal fs-6">${book.first_publish_year}</p>
+                    </div>
+                </div>
+                    `;
+                searchResult.appendChild(div);
+            });
+
+            //clear input value
+            searchField.value = '';
+        });
 }
-searchBook()
-
-
-const displaySearchResult = books => {
-    const searchResult = document.getElementById('search-result');
-    searchResult.textContent = '';
-    if (books.length == 0) {
-       
-    }
-    
-    books.forEach(book => {
-        console.log(book);
-        const div = document.createElement('div');
-        div.classList.add('col');
-        div.innerHTML = `
-        <div class=" card card-body text-start">
-            <p class="fw-bold fs-6">Book Name : <span class="fw-normal fs-5">${book.title}<span><p>
-            <p><span class="fw-bold fs-6">Author Name : <span class="fw-normal fs-6"> ${book.author_name}</p>
-            <p><span class="fw-bold fs-6">Publisher : <span class="fw-normal fs-6"> ${book.publisher_facet}</p>
-            <p><span class="fw-bold fs-6">First Published Year : <span class="fw-normal fs-6">${book.first_publish_year}</p>
-        </div>
-        </div>
-        `;
-        searchResult.appendChild(div);
-    })
-}
-
-
